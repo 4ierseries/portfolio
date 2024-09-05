5467a041ee7a29c91f363867437ead2e4edc4d57
Salma J. <87875257+ssszlma@users.noreply.github.com>
2024-09-05 10:22:06 -0400

    Update presentation.ts

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -15,7 +15,7 @@ const presentation: Presentation = {
   title: "Salma J.",
   description:
       "Hello! I'm Salma, a junior at Boston Collegiate Charter School."
-      + " I build things like **Apps**, **Games**, and projects on the **Web**!. I've also competed at several Hackathons and enjoy hurting my brain with Competitive Programming & Computational Linguistics problems."
+      + " I build things like **Apps**, **Games**, and projects on the **Web**. I've also competed at several Hackathons and enjoy hurting my brain with Competitive Programming & Computational Linguistics problems."
       + " Outside of the technical world I'm also part of my school's Debate team and enjoy designing clothes!"
       + "<br><br> ▪  I'm currently working as a **Software Engineer** at [**Kidogo Productions**](\"https://www.kidogoproductions.com\"). "
       + "<br><br> ▪  Check out **[/projects](\"https://salmaj.me/projects\")** or my GitHub [(@**ssszlma**)](\"https://github.com/ssszlma\").",
