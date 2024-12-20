c1e5460c8ac79822cec95dcde5a35d16f2a93e67
Salma J. <87875257+4ierseries@users.noreply.github.com>
2024-12-20 18:45:11 -0500

    Update presentation.ts
    safewh5jtdsjegbo

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -17,7 +17,7 @@ const presentation: Presentation = {
       "I'm Salma, a junior at Boston Collegiate Charter School."
       + " I have a wide variety of interests, ranging from programming, math, and physics to public policy and debate."
       + " I'm interested in pursuing **optical engineering** and have a deep love for astrophysics! "
-      + "<br><br> I'm currently working as a **Software Engineer** at [Kidogo Productions](\"https://www.kidogoproductions.com\"), and as a **Youth Councilor** for the [Boston Mayor's Youth Council.](\"https://bit.ly/mycboston\")",
+      + "<br><br> I'm currently working as a **Software Developer** at [Kidogo Productions](\"https://www.kidogoproductions.com\"), and as a **Youth Councilor** for the [Boston Mayor's Youth Council.](\"https://bit.ly/mycboston\")",
 
 
 
