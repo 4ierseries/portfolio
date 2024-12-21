916d9a35f53b30bc29a4f812bcd9cc3f8530054c
Salma J. <87875257+4ierseries@users.noreply.github.com>
2024-12-20 20:49:30 -0500

    Update convertAsteriskToStrongTag.ts

--- src/utils/convertAsteriskToStrongTag.ts
+++ src/utils/convertAsteriskToStrongTag.ts
@@ -1,15 +1,13 @@
 export default function convertTextToHtml(str: string): string {
-  // Convert asterisks to bold text with white color
   str = str.replace(
       /\*\*([^\*]+)\*\*/g,
       `<span class="text-white">$1</span>`
   );
 
-  // Replace links with a fixed color (e.g., red) after other conversions
   str = str.replace(
       /\[([^\]]+)\]\("([^"]+)"\)/g,
       (match, text, link) => {
-        const colorClass = 'text-blue-400'; // Set to a fixed red color
+        const colorClass = 'text-blue-400'; 
         return `<a href="${link}" class="${colorClass}">${text}</a>`;
       }
   );
