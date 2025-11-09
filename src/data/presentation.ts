8afde52e57a7347df1a2c3b825f69c9874a011b2
Salma J <87875257+ssszlma@users.noreply.github.com>
2025-11-09 17:59:50 -0500

    aaaaaa

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -1,41 +1,19 @@
-type Social = {
-  label: string;
-  link: string;
-};
-
 type Presentation = {
   // mail: string;
   title: string;
   description: string;
-  socials: Social[];
   profile?: string;
 };
 
 const presentation: Presentation = {
   title: "Salma J.",
   description:
-      "I'm Salma, a senior at Boston Collegiate Charter School."
-      + " I've been programming for ~4 years now, and am also interested in math, physics, linguistics, and policy. I'm not sure about what I want to do in the future, but for now I've been pursuing all these interests. :)"
-  + "<br/><br/> For projects I've worked on, check out my GitHub: [**@4ierseries**](https://github.com/4ierseries).",
+      "I'm Salma, a senior at Boston Collegiate Charter School who's been programming inconsistently for ~5+ years now." +
+      " I'm interested in optical space instrumentation which combines my interests in math, engineering, and astronomy. " +
+      " In my free time I enjoy reading manhwa and playing tetris. :)" +
 
+      "<br/><br/> For my previous work, see my Github [**@4ierseries**](https://github.com/4ierseries) and LinkedIn [**/in/4ierseries/**](https://www.linkedin.com/in/4ierseries/).",
 
-
-
-
-  socials: [
-    {
-      label: "Github",
-      link: "https://github.com/4ierseries",
-    },
-    {
-      label: "LinkedIn",
-      link: "https://www.linkedin.com/in/4ierseries/",
-    },
-    {
-      label: "Email",
-      link: "mailto:salmaabdijama@gmail.com",
-    },
-  ],
 };
 
 export default presentation;
