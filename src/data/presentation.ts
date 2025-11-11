dba1701b8daa9caf54b02178e870940444dc800a
salm <87875257+4ierseries@users.noreply.github.com>
2025-11-11 15:52:03 -0500

    Revise interests in presentation description
    Updated presentation description to reflect a change in interests.

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -9,7 +9,7 @@ const presentation: Presentation = {
   title: "Salma J.",
   description:
       "I'm Salma, a senior at Boston Collegiate Charter School who's been programming inconsistently for ~5+ years now." +
-      " I'm interested in optical space instrumentation which combines my interests in math, engineering, and astronomy. " +
+      " I'm interested in optical space instrumentation which combines my interests in engineering and astronomy. " +
       " In my free time I enjoy reading manhwa and playing tetris. :)" +
 
       "<br/><br/> For my previous work, see my Github [**@4ierseries**](https://github.com/4ierseries) and LinkedIn [**/in/4ierseries/**](https://www.linkedin.com/in/4ierseries/).",
